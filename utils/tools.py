a6ab76610a53d3b872aba1707ff753317655472e
CHARLES Jean-Luc <jean-luc.charles@ensam.eu>
2020-12-30 15:50:11 +0100

    still on my way...

--- utils/tools.py
+++ utils/tools.py
@@ -1,5 +1,9 @@
+import numpy as np
 import matplotlib.pyplot as plt
-
+import pandas as pd
+from seaborn import heatmap
+from sklearn.metrics import confusion_matrix
+    
 def plot_loss_accuracy(history):
     '''Plot training & validation loss & accuracy values, giving an argument
        'history' of type 'tensorflow.python.keras.callbacks.History'. '''
@@ -38,3 +42,14 @@ def plot_images(image_array, r, L, C):
         plt.imshow(image_array[r+i], cmap='gray')
         plt.xticks([]); plt.yticks([])
  
+def show_cm_mnist(target, results, classes):
+    # y : actual answers (vector of int)
+    # results : the trained network answers (array of 'one-hot' coded vectors)
+    # classes : the liste of possible answers
+    predicted = np.argmax(results, axis=-1) # tableau d'entiers entre 0 et 9 (argmax de chaque vecteur 'one-shot')
+
+    cm = confusion_matrix(target, predicted)
+    df_cm = pd.DataFrame(cm, index=classes, columns=classes)
+    plt.figure(figsize=(11,9))
+    heatmap(df_cm, annot=True);
+    
